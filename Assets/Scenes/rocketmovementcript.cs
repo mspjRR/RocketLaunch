41cb8cacba93380d7d8b92b77b7f4b4cb46ef1f2
MSPJ\ramar <ramarao714@gmail.com>
2021-04-25 14:56:50 +0530

    test run

---
+++ Assets/Scenes/rocketmovementcript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocketmovementcript : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializedField]
+    float thrust;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RocketFly();
+    }
+    private void RocketFly()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rb.AddRelativeForce(Vector3.up * thrust);
+            { 
+                print("up"); 
+            }
+        }
+        else
+        {
+
+        }
+    }
+}
